5da50479ff77b582d0a4813ee1ced99b19fa0bb0
iwearthecrowns <iwearthecrowns@users.noreply.github.com>
2020-09-01 10:11:19 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,126 @@
+sprites.onCreated(SpriteKind.Enemy, function (sprite) {
+    sprite.setVelocity(-50, 0)
+    sprite.setFlag(SpriteFlag.AutoDestroy, true)
+    if (Math.percentChance(50)) {
+        sprite.setImage(img`
+            . . f f f . . . . . . . . . . . 
+            f f f c c . . . . . . . . f f f 
+            f f c c c . c c . . . f c b b c 
+            f f c 3 c c 3 c c f f b b b c . 
+            f f c 3 b c 3 b c f b b c c c . 
+            f c b b b b b b c f b c b c c . 
+            c c 1 b b b 1 b c b b c b b c . 
+            c b b b b b b b b b c c c b c . 
+            c b 1 f f 1 c b b c c c c c . . 
+            c f 1 f f 1 f b b b b f c . . . 
+            f f f f f f f b b b b f c . . . 
+            f f 2 2 2 2 f b b b b f c c . . 
+            . f 2 2 2 2 2 b b b c f . . . . 
+            . . f 2 2 2 b b b c f . . . . . 
+            . . . f f f f f f f . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+    } else {
+        sprite.setImage(img`
+            . . . . . c c c c c c c . . . . 
+            . . . . c 6 7 7 7 7 7 6 c . . . 
+            . . . c 7 c 6 6 6 6 c 7 6 c . . 
+            . . c 6 7 6 f 6 6 f 6 7 7 c . . 
+            . . c 7 7 7 7 7 7 7 7 7 7 c . . 
+            . . f 7 8 1 f f 1 6 7 7 7 f . . 
+            . . f 6 f 1 f f 1 f 7 7 7 f . . 
+            . . . f f 2 2 2 2 f 7 7 6 f . . 
+            . . c c f 2 2 2 2 7 7 6 f c . . 
+            . c 7 7 7 7 7 7 7 7 c c 7 7 c . 
+            c 7 1 1 1 7 7 7 7 f c 6 7 7 7 c 
+            f 1 1 1 1 1 7 6 f c c 6 6 6 c c 
+            f 1 1 1 1 1 1 6 6 c 6 6 6 c . . 
+            f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+            . f 6 1 1 1 1 1 6 6 6 6 c . . . 
+            . . f f c c c c c c c c . . . . 
+            `)
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . 3 3 b 3 3 d d 3 3 . . 
+        . . . . 3 1 1 d 3 d 1 1 1 1 3 . 
+        . . . 3 d 1 1 1 d 1 1 1 d 3 1 3 
+        . . 3 d d 1 1 1 d d 1 1 1 3 3 3 
+        . 3 1 1 d 1 1 1 1 d d 1 1 b . . 
+        . 3 1 1 1 d 1 1 1 1 1 d 1 1 3 . 
+        . b d 1 1 1 d 1 1 1 1 1 1 1 3 . 
+        . 4 b 1 1 1 1 d d 1 1 1 1 d 3 . 
+        . 4 4 d 1 1 1 1 1 1 d d d b b . 
+        . 4 d b d 1 1 1 1 1 1 1 1 3 . . 
+        4 d d 5 b d 1 1 1 1 1 1 1 3 . . 
+        4 5 d 5 5 b b d 1 1 1 1 d 3 . . 
+        4 5 5 d 5 5 d b b b d d 3 . . . 
+        4 5 5 5 d d d d 4 4 b 3 . . . . 
+        . 4 5 5 5 4 4 4 . . . . . . . . 
+        . . 4 4 4 . . . . . . . . . . . 
+        `, mySprite, 50, 0)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    info.changeScoreBy(1)
+    sprite.startEffect(effects.spray)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    scene.cameraShake(4, 500)
+    info.changeLifeBy(-1)
+})
+let enemySprite: Sprite = null
+let projectile: Sprite = null
+let mySprite: Sprite = null
+scene.setBackgroundColor(0)
+mySprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+info.setLife(3)
+game.onUpdateInterval(500, function () {
+    enemySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . f f f f . . . . . . . . . . 
+        . . . . . . . . f f 1 1 1 1 f f . . . . . . . . 
+        . . . . . . . f b 1 1 1 1 1 1 b f . . . . . . . 
+        . . . . . . . f 1 1 1 1 1 1 1 1 f . . . . . . . 
+        . . . . . . f d 1 1 1 1 1 1 1 1 d f . . . . . . 
+        . . . . . . f d 1 1 1 1 1 1 1 1 d f . . . . . . 
+        . . . . . . f d d d 1 1 1 1 d d d f . . . . . . 
+        . . . . . . f b d b f d d f b d b f . . . . . . 
+        . . . . . . f c d c f 1 1 f c d c f . . . . . . 
+        . . . . . . . f b 1 1 1 1 1 1 b f . . . . . . . 
+        . . . . . . f f f c d b 1 b d f f f f . . . . . 
+        . . . . f c 1 1 1 c b f b f c 1 1 1 c f . . . . 
+        . . . . f 1 b 1 b 1 f f f f 1 b 1 b 1 f . . . . 
+        . . . . f b f b f f f f f f b f b f b f . . . . 
+        . . . . . . . . . f f f f f f . . . . . . . . . 
+        . . . . . . . . . . . f f f . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    enemySprite.setPosition(randint(0, 160), randint(0, 120))
+})
